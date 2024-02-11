b56759c2b84fe1343e31a495525a68115e4a4dc7
Sujit-Pawar-9 <sujitpawar18599@gmail.com>
2024-02-12 01:07:50 +0530

    update

--- SpringBootSkillLink/src/main/java/com/demo/model/UserRequirements.java
+++ SpringBootSkillLink/src/main/java/com/demo/model/UserRequirements.java
@@ -4,66 +4,82 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "userrequirements")
 public class UserRequirements {
-	@Id
-	private int UserId;
-	private String Skills;
-	private double Wages;
-	private String Address;
-	private String Date;
-	public UserRequirements() {
-		super();
-	}
-	public UserRequirements(String skills, double wages, String address, String date) {
-		super();
-		Skills = skills;
-		Wages = wages;
-		Address = address;
-		Date = date;
-	}
-	public int getUserId() {
-		return UserId;
-	}
-	public void setUserId(int userId) {
-		UserId = userId;
-	}
-	public String getSkills() {
-		return Skills;
-	}
-	public void setSkills(String skills) {
-		Skills = skills;
-	}
-	public double getWages() {
-		return Wages;
-	}
-	public void setWages(double wages) {
-		Wages = wages;
-	}
-	public String getAddress() {
-		return Address;
-	}
-	public void setAddress(String address) {
-		Address = address;
-	}
-	public String getDate() {
-		return Date;
-	}
-	public void setDate(String date) {
-		Date = date;
-	}
-	@Override
-	public String toString() {
-		return "UserRequirements [UserId=" + UserId + ", Skills=" + Skills + ", Wages=" + Wages + ", Address=" + Address
-				+ ", Date=" + Date + "]";
-	}
-	
 
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
 
+    private String skills;
+    private double wages;
+    private String address;
+    private String date;
+
+    public UserRequirements() {
+        super();
+    }
+
+    public UserRequirements(String skills, double wages, String address, String date) {
+        super();
+        this.skills = skills;
+        this.wages = wages;
+        this.address = address;
+        this.date = date;
+    }
+
+    // Getters and Setters
+
+    @Column(name = "user_id")
+    public int getUserId() {
+        return userId;
+    }
+
+    private void setUserId(int userId) {
+        //this.userId = userId;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public double getWages() {
+        return wages;
+    }
+
+    public void setWages(double wages) {
+        this.wages = wages;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequirements [userId=" + userId + ", skills=" + skills + ", wages=" + wages + ", address="
+                + address + ", date=" + date + "]";
+    }
 }
